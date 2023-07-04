7b0d3476cd80a87ecfdf8184343225bc8e597939
Миша Попов <misha@air-misa.home>
2023-07-04 15:26:13 -0400

    create models Sneakers and Caregories

--- backend/tstore/tstore_App/models.py
+++ backend/tstore/tstore_App/models.py
@@ -6,6 +6,19 @@ class Sneakers(models.Model):
     description = models.CharField(max_length=100)
     image = models.URLField(blank=True)
     in_stock = models.BooleanField(blank=True)
+    category = models.ForeignKey('Categories', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Sneakers"
+        verbose_name_plural = "Sneakers"
+
+class Categories(models.Model):
+    name = models.CharField(max_length=40)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
 
 
 
