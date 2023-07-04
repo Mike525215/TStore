19885fe50ccd56931689ac6cd8078c47fcbff8d3
Миша Попов <misha@air-misa.home>
2023-07-04 00:41:19 -0400

    add more styles for Categories

--- frontend/tstore_app/src/components/Categories/Categories.js
+++ frontend/tstore_app/src/components/Categories/Categories.js
@@ -3,24 +3,22 @@ import s from './Categories.module.css';
 const Categories = () => {
     return (
         <div className={s.categories}>
-            <section className={s.catList}>
-                <ul>
-                    <li><a href="#">Jordan 1 Mid</a></li>
-                    <li><a href="#">Jordan 1 Low</a></li>
-                    <li><a href="#">Jordan 4</a></li>
-                    <li><a href="#">Yeezy Boost</a></li>
-                </ul>
-            </section>
-            <section className={s.sortBlock}>
-                <span className={s.sortText}>Сортировка по:</span>
-                <select className={s.selectBlock}>
-                    <option disable></option>
-                    <option value="1">Price(increase)</option>
-                    <option value="2">Price(less)</option>
-                    <option value="3">Title(up)</option>
-                    <option value="4">Title(down)</option>
-                </select>
-            </section>
+                <section className={s.catList}>
+                    <ul>
+                        <li><a href="#">Jordan 1</a></li>
+                        <li><a href="#">Jordan 4</a></li>
+                        <li><a href="#">Yeezy Boost</a></li>
+                    </ul>
+                </section>
+                <section className={s.sortBlock}>
+                    <select className={s.selectBlock}>
+                        <option disable>Sorting by</option>
+                        <option value="1">Price(increase)</option>
+                        <option value="2">Price(less)</option>
+                        <option value="3">Title(up)</option>
+                        <option value="4">Title(down)</option>
+                    </select>
+                </section>
         </div>
     );
 }
