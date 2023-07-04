11ef3bcea92a936fc4d19f3bf56a1d3712433b44
Миша Попов <misha@air-misa.home>
2023-07-04 15:13:41 -0400

    create model Sneakers

--- backend/tstore/tstore_App/models.py
+++ backend/tstore/tstore_App/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Sneakers(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField(blank=True)
+    description = models.CharField(max_length=100)
+    image = models.URLField(blank=True)
+    in_stock = models.BooleanField(blank=True)
+
+
+
