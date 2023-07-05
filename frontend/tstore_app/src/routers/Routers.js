453829aeb6242495277e3245673a2624f9c2671f
Миша Попов <misha@air-misa.home>
2023-07-05 16:57:21 -0400

    create new components Login and SignUp. Create routes for new components

--- frontend/tstore_app/src/routers/Routers.js
+++ frontend/tstore_app/src/routers/Routers.js
@@ -1,5 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Home} from '../components/Home/Home';
+import {Login} from '../components/AuthComponent/Login';
+import {SignUp} from '../components/AuthComponent/SignUp';
 import {SneakersDetail} from '../components/SneakersDetail/SneakersDetail';
 import {useState, useEffect, createContext} from 'react';
 import {services} from '../services/services.js';
@@ -26,6 +28,8 @@ const Routers = () => {
             <Routes>
                 <Route element={<Home />} path='/sneakers/' />
                 <Route element={<Home />} path='/sneakers/:cat' />
+                <Route element={<Login />} path='/auth/login/' />
+                <Route element={<SignUp />} path='/auth/signup/' />
                 <Route element={<SneakersDetail />} path='/sneakers/:cat/:id/' />
                 <Route element={<Home />} path='/cart/' />
                 <Route element={<div><h1>Page not Found 404</h1></div>} path="*" />
