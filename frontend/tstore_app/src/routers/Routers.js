2456911b98d259f55b19f544f89d9e69197c4597
Миша Попов <misha@air-misa.home>
2023-07-05 03:00:28 -0400

    include new component SneakersDetail in app

--- frontend/tstore_app/src/routers/Routers.js
+++ frontend/tstore_app/src/routers/Routers.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Home} from '../components/Home/Home';
+import {SneakersDetail} from '../components/SneakersDetail/SneakersDetail';
 
 const Routers = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<Home />} path='/sneakers/' />
+                <Route element={<SneakersDetail />} path='/sneakers/:id/' />
                 <Route element={<Home />} path='/cart/' />
                 <Route element={<div><h1>Page not Found 404</h1></div>} path="*" />
             </Routes>
