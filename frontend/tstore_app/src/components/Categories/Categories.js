e556ce04d1d040dab5e51cf13e3eafd1a69780d3
Миша Попов <misha@air-misa.home>
2023-07-05 03:19:08 -0400

    change a on Link. Also change path

--- frontend/tstore_app/src/components/Categories/Categories.js
+++ frontend/tstore_app/src/components/Categories/Categories.js
@@ -1,18 +1,19 @@
 import s from './Categories.module.css';
+import {Link} from 'react-router-dom';
 
 const Categories = () => {
     return (
         <div className={s.categories}>
                 <section className={s.catList}>
                     <ul>
-                        <li><a href="#">Jordan 1</a></li>
-                        <li><a href="#">Jordan 4</a></li>
-                        <li><a href="#">Yeezy Boost</a></li>
+                        <li><Link to="/sneakers/jordan-1/">Jordan 1</Link></li>
+                        <li><Link to="/sneakers/jordan-4/">Jordan 4</Link></li>
+                        <li><Link to="/sneakers/yeezy-boost/">Yeezy Boost</Link></li>
                     </ul>
                 </section>
                 <section className={s.sortBlock}>
                     <select className={s.selectBlock}>
-                        <option disable>Sorting by</option>
+                        <option disable="true">Sorting by</option>
                         <option value="1">Price(up)</option>
                         <option value="2">Price(down)</option>
                         <option value="3">Title(up)</option>
