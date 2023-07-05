19cc1f941410b243332142ed542ce004de185210
Миша Попов <misha@air-misa.home>
2023-07-05 03:11:01 -0400

    include context and params to create SneakersDetail component

--- frontend/tstore_app/src/components/SneakersDetail/SneakersDetail.js
+++ frontend/tstore_app/src/components/SneakersDetail/SneakersDetail.js
@@ -1,6 +1,15 @@
 import s from './SneakersDetail.module.css';
+import {useContext} from 'react';
+import {useParams} from 'react-router-dom';
+import {SneakersList} from '../../routers/Routers.js';
+import {Item} from '../Trainers/Item/Item';
 
 const SneakersDetail = (props) => {
-    return ();
+    const { id } = useParams();
+    const { sneakers } = useContext(SneakersList);
+    return (
+        <Item item={sneakers[id - 1]} />
+    );
 }
 
+export {SneakersDetail};
