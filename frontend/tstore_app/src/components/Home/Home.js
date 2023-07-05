b5abe4d5c7bf07f33a771d0c30c136a93a59b51f
Миша Попов <misha@air-misa.home>
2023-07-05 14:19:54 -0400

    include auth-system in app

--- frontend/tstore_app/src/components/Home/Home.js
+++ frontend/tstore_app/src/components/Home/Home.js
@@ -2,16 +2,19 @@ import {Header} from '../Header/Header';
 import {Categories} from '../Categories/Categories';
 import {Trainers} from '../Trainers/Trainers';
 import s from './Home.module.css';
-
+import {AuthComponent} from '../AuthComponent/AuthComponent';
 
 const Home = () => {
 
     return (
-        <main>
-            <Header />
-            <Categories />
-            <Trainers />
-        </main>
+        <div>
+            <AuthComponent />
+            <main>
+                <Header />
+                <Categories />
+                <Trainers />
+            </main>
+        </div>
     );
 }
 
