c78ce773f4a165e144b3ce9ee71669e99a1e1ee9
Миша Попов <misha@air-misa.home>
2023-07-05 02:59:49 -0400

    image was transformed in a Link

--- frontend/tstore_app/src/components/Trainers/Item/Item.js
+++ frontend/tstore_app/src/components/Trainers/Item/Item.js
@@ -1,9 +1,12 @@
 import s from './Item.module.css';
+import {Link} from 'react-router-dom';
 
 const Item = (props) => {
     return (
         <div className={s.cardItem}>
-            <img className={s.cardImage} src={props.item.image} alt="jordan" />
+            <Link to={"/sneakers/" + props.item.id + "/"}>
+                <img className={s.cardImage} src={props.item.image} alt="jordan" />
+            </Link>
             <span className={s.itemName}>{props.item.title}</span>
             <span className={s.itemDescription}>{props.item.description}</span>
             <span className={s.itemPrice}>${props.item.price}</span>
