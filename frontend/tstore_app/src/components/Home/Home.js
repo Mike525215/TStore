9b9f856a85455aca0df8dfbdf55310e2bf72012c
Миша Попов <misha@air-misa.home>
2023-07-05 03:09:30 -0400

    relocate context from Home to Routers. Also relocate feature with get sneakers

--- frontend/tstore_app/src/components/Home/Home.js
+++ frontend/tstore_app/src/components/Home/Home.js
@@ -2,33 +2,16 @@ import {Header} from '../Header/Header';
 import {Categories} from '../Categories/Categories';
 import {Trainers} from '../Trainers/Trainers';
 import s from './Home.module.css';
-import {useState, useEffect, createContext} from 'react';
-import {services} from '../../services/services.js';
 
-export const SneakersList = createContext();
 
 const Home = () => {
 
-    const [sneakers, setSneakers] = useState([]);
-
-    const sneakersRender = async () => {
-        const request = await services.sneakersList();
-        const response = await request.json();
-        setSneakers(response);
-    }
-
-    useEffect(() => {
-        sneakersRender();
-    }, []);
-
     return (
-        <SneakersList.Provider value={{sneakers}}>
-            <main>
-                <Header />
-                <Categories />
-                <Trainers />
-            </main>
-        </SneakersList.Provider>
+        <main>
+            <Header />
+            <Categories />
+            <Trainers />
+        </main>
     );
 }
 
--- frontend/tstore_app/src/routers/Routers.js
+++ frontend/tstore_app/src/routers/Routers.js
@@ -1,9 +1,27 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Home} from '../components/Home/Home';
 import {SneakersDetail} from '../components/SneakersDetail/SneakersDetail';
+import {useState, useEffect, createContext} from 'react';
+import {services} from '../services/services.js';
+
+export const SneakersList = createContext();
 
 const Routers = () => {
+
+    const [sneakers, setSneakers] = useState([]);
+
+    const sneakersRender = async () => {
+        const request = await services.sneakersList();
+        const response = await request.json();
+        setSneakers(response);
+    }
+
+    useEffect(() => {
+        sneakersRender();
+    }, []);
+
     return (
+        <SneakersList.Provider value={{sneakers}}>
         <BrowserRouter>
             <Routes>
                 <Route element={<Home />} path='/sneakers/' />
@@ -12,6 +30,7 @@ const Routers = () => {
                 <Route element={<div><h1>Page not Found 404</h1></div>} path="*" />
             </Routes>
         </BrowserRouter>
+        </SneakersList.Provider>
     );
 }
 
