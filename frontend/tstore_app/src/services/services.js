e4af6a1ed897363b29f6aaa32bac88402a6a5c8f
Миша Попов <misha@air-misa.home>
2023-07-06 03:25:00 -0400

    microservices signUp, login were created

--- frontend/tstore_app/src/services/services.js
+++ frontend/tstore_app/src/services/services.js
@@ -2,6 +2,27 @@ const services = {
     async sneakersList() {
         const request = await fetch("http://127.0.0.1:8000/api/v1/sneakers/");
         return request;
+    },
+    async signUp(username, password, email) {
+        const body = new FormData();
+        body.append("username", username);
+        body.append("password", password);
+        body.append("email", email);
+        const request = await fetch("http://127.0.0.1:8000/api/v1/auth/users/", {
+            method: "POST",
+            body: body
+        });
+        return request;
+    },
+    async login(username, password) {
+        const body = new FormData();
+        body.append("username", username);
+        body.append("password", password);
+        const request = await fetch("http://127.0.0.1:8000/auth/token/login/", {
+            method: "POST",
+            body: body
+        });
+        return request;
     }
 };
 
