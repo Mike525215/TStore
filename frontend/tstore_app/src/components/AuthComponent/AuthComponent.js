37250036b818f82bc61afa6794844fd5dc82647a
Миша Попов <misha@air-misa.home>
2023-07-06 03:27:48 -0400

    create render html depends on users authorization. Also add some styles

--- frontend/tstore_app/src/components/AuthComponent/AuthComponent.js
+++ frontend/tstore_app/src/components/AuthComponent/AuthComponent.js
@@ -1,13 +1,27 @@
 import s from './AuthComponent.module.css';
+import {useContext} from 'react';
 import {Link} from 'react-router-dom';
+import {SneakersList} from '../../routers/Routers';
 
 const AuthComponent = () => {
-    return (
-        <div className={s.authButtons}>
-            <Link className={s.btn}>Sign up</Link>
-            <Link className={s.authBtn}>Authorization</Link>
-        </div>
-    );
+
+    const context = useContext(SneakersList);
+    let html = '';
+
+        if (context.token) {
+            html =
+                <div className={s.authButtons}>
+                    <div className={s.username}>{context.username}</div>
+                    <button className={s.logoutBtn}>Logout</button>
+                </div>
+        } else {
+            html =
+                <div className={s.authButtons}>
+                    <Link className={s.btn} to="/auth/signup/">Sign up</Link>
+                    <Link className={s.authBtn} to="/auth/login/">Authorization</Link>
+                </div>
+        }
+    return html;
 }
 
 export {AuthComponent};
