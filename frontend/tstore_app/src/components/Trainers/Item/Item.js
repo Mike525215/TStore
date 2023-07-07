9f325f3dfa02a39601b85521b338dbdb2c63b4db
Миша Попов <misha@air-misa.home>
2023-07-07 17:59:47 -0400

    change Link path. Also add new styles

--- frontend/tstore_app/src/components/Trainers/Item/Item.js
+++ frontend/tstore_app/src/components/Trainers/Item/Item.js
@@ -1,10 +1,20 @@
 import s from './Item.module.css';
-import {Link} from 'react-router-dom';
-
+import {Link, useParams} from 'react-router-dom';
 const Item = (props) => {
+    let { cat } = useParams();
+
+    if (!cat) {
+        if (props.item.id === 1) {
+            cat = 'jordan-1';
+        } else if (props.item.id === 2) {
+            cat = 'jordan-4';
+        } else {
+            cat = 'yeezy-boost';
+        }
+    }
     return (
         <div className={s.cardItem}>
-            <Link to={"/sneakers/" + props.item.category + "/" + props.item.id + "/"}>
+            <Link to={"/sneakers/" + cat + "/" + props.item.id + "/"}>
                 <img className={s.cardImage} src={props.item.image} alt="jordan" />
             </Link>
             <span className={s.itemName}>{props.item.title}</span>
