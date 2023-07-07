373f2b1430cf186e1f72605477a5255f0ae50c11
Миша Попов <misha@air-misa.home>
2023-07-06 23:41:37 -0400

    was added feature back on home page. Also was added some styles for SignUp

--- frontend/tstore_app/src/components/SneakersDetail/SneakersDetail.js
+++ frontend/tstore_app/src/components/SneakersDetail/SneakersDetail.js
@@ -1,6 +1,6 @@
 import s from './SneakersDetail.module.css';
 import {useContext} from 'react';
-import {useParams} from 'react-router-dom';
+import {useParams, Link} from 'react-router-dom';
 import {SneakersList} from '../../routers/Routers.js';
 import {Item} from '../Trainers/Item/Item';
 
@@ -8,7 +8,10 @@ const SneakersDetail = (props) => {
     const { id } = useParams();
     const { sneakers } = useContext(SneakersList);
     return (
-        <Item item={sneakers[id - 1]} />
+        <>
+            <Item item={sneakers[id - 1]} />
+            <Link to="/sneakers/">Main page</Link>
+        </>
     );
 }
 
