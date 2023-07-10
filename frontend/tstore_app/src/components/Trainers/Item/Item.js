26214d4920bf480b85acc11baf7cb70b2e4e2225
Миша Попов <misha@MacBook-Air-Misa.local>
2023-07-10 18:28:38 -0400

    make add button interactive

--- frontend/tstore_app/src/components/Trainers/Item/Item.js
+++ frontend/tstore_app/src/components/Trainers/Item/Item.js
@@ -1,12 +1,27 @@
 import s from './Item.module.css';
 import {Link, useParams} from 'react-router-dom';
+import {useContext} from 'react';
+import {SneakersList} from '../../../routers/Routers.js';
+import {services} from '../../../services/services.js';
 
 const Item = (props) => {
     let { cat } = useParams();
-
+    const { username, userID } = useContext(SneakersList);
     if (!cat) {
         cat = props.item.category;
     }
+
+    let addBtn = '';
+
+    if (username !== '') {
+        addBtn = <button className={s.addBtn}
+                         onClick={
+                             async() => {
+                                await services.addSneakers(props.item.id, userID);
+                             }
+                         }>Add to Cart</button>;
+    }
+
     return (
         <div className={s.cardItem}>
             <Link to={"/sneakers/" + cat + "/" + props.item.id + "/"}>
@@ -15,6 +30,7 @@ const Item = (props) => {
             <span className={s.itemName}>{props.item.title}</span>
             <span className={s.itemDescription}>{props.item.description}</span>
             <span className={s.itemPrice}>${props.item.price}</span>
+            {addBtn}
         </div>
     );
 }
